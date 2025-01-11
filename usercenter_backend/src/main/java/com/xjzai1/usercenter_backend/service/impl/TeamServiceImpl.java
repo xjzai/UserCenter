5c702bf2b733a9004642312efc537a7cbca9bd3d
xjzai1 <gys54818901@gmail.com>
2025-01-11 19:43:05 +0800

    完成了addTeam方法

--- usercenter_backend/src/main/java/com/xjzai1/usercenter_backend/service/impl/TeamServiceImpl.java
+++ usercenter_backend/src/main/java/com/xjzai1/usercenter_backend/service/impl/TeamServiceImpl.java
@@ -38,8 +38,72 @@ public class TeamServiceImpl extends ServiceImpl<TeamMapper, Team>
     @Transactional(rollbackFor = Exception.class)
     @Override
     public Integer addTeam(Team team, User loginUser) {
-
-        return null;
+//1. 请求参数是否为空？ 2. 是否登录，未登录不允许创建
+        if (team == null || loginUser == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        final int userId = loginUser.getId();
+        //3. 校验信息
+        //  a. 队伍人数 > 1 且 <= 20
+        Integer maxNum = Optional.ofNullable(team.getMaxNum()).orElse(0);
+        if (maxNum < 1 || maxNum > 20) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "队伍人数不符合要求");
+        }
+        //  b. 队伍标题 <= 20
+        String name = team.getName();
+        if (StringUtils.isBlank(name) || name.length() > 20) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "队伍标题不满足要求");
+        }
+        //  c. 描述 <= 512
+        String description = team.getDescription();
+        if (StringUtils.isNotBlank(description) && description.length() > 512) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "队伍描述过长");
+        }
+        //  d. status 是否公开（int）不传默认为 0（公开）
+        int status = Optional.ofNullable(team.getStatus()).orElse(0);
+        TeamStatusEnum statusEnum = TeamStatusEnum.getEnumByValue(status);
+        if (statusEnum == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "队伍状态不满足要求");
+        }
+        //  e. 如果 status 是加密状态，一定要有密码，且密码 <= 32
+        // todo 不管是不是加密状态都会存入密码
+        String password = team.getPassword();
+        if (TeamStatusEnum.SECRET.equals(statusEnum)) {
+            if (StringUtils.isBlank(password) || password.length() > 32) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码设置不正确");
+            }
+        }
+        //  f. 超时时间 > 当前时间
+        Date expireTime = team.getExpireTime();
+        if (new Date().after(expireTime)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "超时时间 > 当前时间");
+        }
+        //  g. 校验用户最多创建 5 个队伍
+        // todo 有 bug，可能同时创建 100 个队伍
+        QueryWrapper<Team> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", userId);
+        long hasTeamNum = teamMapper.selectCount(queryWrapper);
+        if (hasTeamNum >= 5) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户最多创建 5 个队伍");
+        }
+        //4. 插入队伍信息到队伍表
+        team.setId(null);
+        team.setUserId(userId);
+        boolean result = this.save(team);
+        Integer teamId = team.getId();
+        if (!result || teamId == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "创建队伍失败");
+        }
+        //5. 插入用户 => 队伍关系到关系表
+        UserTeam userTeam = new UserTeam();
+        userTeam.setUserId(userId);
+        userTeam.setTeamId(teamId);
+        userTeam.setJoinTime(new Date());
+        result = userTeamService.save(userTeam);
+        if (!result) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "创建队伍失败");
+        }
+        return userId;
     }
 }
 
